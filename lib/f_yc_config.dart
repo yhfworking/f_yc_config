2e529f3683eacee387724079e40723afc3246165
yhfwork <mm20102010mm>
2022-11-03 05:17:14 +0800

    update

--- lib/f_yc_config.dart
+++ lib/f_yc_config.dart
@@ -1,7 +1,9 @@
 library f_yc_config;
 
+import 'package:f_yc_config/f_yc_config.dart';
+
+export './f_yc_config.dart';
 export './f_yc_config_configurator.dart';
-export './f_yc_config_initializer.dart';
 export './config/f_yc_config_all_config.dart';
 export './config/f_yc_config_base_config.dart';
 export './config/f_yc_config_common_config.dart';
@@ -10,3 +12,13 @@ export './config/f_yc_config_pangrowth_config.dart';
 export './config/f_yc_config_pangle_config.dart';
 export './config/f_yc_config_mtj_config.dart';
 export './config/f_yc_config_wechat_config.dart';
+
+/// 初始化
+class FYcConfig {
+  static initializer({
+    FYcConfigAllConfig? allConfig,
+    String configId = GLOBAL_CONFIG_ID,
+  }) {
+    FYcConfigConfigurator.instance.register(allConfig, configId: configId);
+  }
+}
